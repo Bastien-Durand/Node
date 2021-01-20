dc4551d1eef3c688acf706ec3f50bdbf8ee44d8c
Bastien Durand <basilexpeditions@gmail.com>
2021-01-20 14:15:16 +1100

    Arrow functions challenge + Refactoring with arrow functions

--- notes-app/notes.js
+++ notes-app/notes.js
@@ -1,15 +1,10 @@
 const fs = require("fs");
 const chalk = require("chalk");
 
-const getNotes = function () {
-  return "Your notes...";
-};
-
-const addNote = function (title, body) {
+const getNotes = () => "Your notes...";
+const addNote = (title, body) => {
   const notes = loadNotes();
-  const duplicateNotes = notes.filter(function (note) {
-    return note.title === title;
-  });
+  const duplicateNotes = notes.filter((note) => note.title === title);
 
   if (duplicateNotes.length === 0) {
     notes.push({

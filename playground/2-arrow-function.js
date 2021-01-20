926f10e7865b9be59a9ed25b706401159e2412ce
Bastien Durand <basilexpeditions@gmail.com>
2021-01-20 13:05:30 +1100

    Arrow function practice

---
+++ playground/2-arrow-function.js
@@ -0,0 +1,23 @@
+// const square = (x) => x * x;
+
+// console.log(square(3));
+
+// const squared = (x) => {
+//   return x * x;
+// };
+
+// console.log(squared(4));
+
+const event = {
+  name: "Birthday Party",
+  guestList: ["Andrew", "Jen", "Mike"],
+  printGuestList() {
+    console.log("Guest list for " + this.name);
+
+    this.guestList.forEach((guest) => {
+      console.log(`${guest} is attending ` + this.name);
+    });
+  },
+};
+
+event.printGuestList();

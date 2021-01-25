a49f786e67836260b91730a9ec74c18ef31c690c
Bastien Durand <basilexpeditions@gmail.com>
2021-01-25 15:45:34 +1100

    refactored asynchronous htpp request

--- weather-app/app.js
+++ weather-app/app.js
@@ -3,10 +3,9 @@ const request = require("request");
 const URL =
   "http://api.weatherstack.com/current?access_key=fdd2af8236f75ed878fc09269407c622&query=37.8267,-122.4233";
 
-request(URL, function (error, response, body) {
-  console.error("error:", error); // Print the error if one occurred
-  console.log("statusCode:", response && response.statusCode); // Print the response status code if a response was received
-  console.log("body:", body); // Print the HTML for the Google homepage.
+request({ url: URL }, (error, response) => {
+  const data = JSON.parse(response.body);
+  console.log(data.current);
 });
 
 console.log("Finished before callback");

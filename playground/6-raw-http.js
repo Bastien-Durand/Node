a9ec9f39deeb107bb1102743dbf9b3486a820049
Bastien Durand <basilexpeditions@gmail.com>
2021-02-01 15:33:17 +1100

    Practice using core node module 'http'

---
+++ playground/6-raw-http.js
@@ -0,0 +1,25 @@
+const { clear } = require("console");
+
+const http = require("http");
+
+const URL =
+  "http://api.weatherstack.com/current?access_key=fdd2af8236f75ed878fc09269407c622&query=45,-75&units=f";
+
+const request = http.request(URL, (response) => {
+  let data = "";
+
+  response.on("data", (chunk) => {
+    data = data + chunk.toString();
+  });
+
+  response.on("end", () => {
+    const body = JSON.parse(data);
+    console.log(body);
+  });
+});
+
+request.on("error", (error) => {
+  console.log("an Error", error);
+});
+
+request.end();

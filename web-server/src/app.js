128f71aa7ad4f83bfdf2b60858669bf41e8a7c18
Bastien Durand <basilexpeditions@gmail.com>
2021-02-02 11:53:13 +1100

    Raw http request practice without a library, + changes to root web server

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -1,6 +1,11 @@
 const express = require("express");
+const path = require("path");
 
 const app = express();
+
+const publicDirPath = path.join(__dirname, "../public");
+
+app.use(express.static(publicDirPath));
 
 app.get("/", (req, res) => {
   res.send("Hello express!");

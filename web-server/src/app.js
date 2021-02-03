692fd1c9f7561c9da10e2bb2b459ff66b1bb504b
Bastien Durand <basilexpeditions@gmail.com>
2021-02-03 12:31:20 +1100

    Starting to use templating language

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -1,13 +1,20 @@
 const express = require("express");
 const path = require("path");
 
-const PORT = 3000;
-
 const app = express();
 
 //This is the root being served as a static directory
-
 app.use(express.static(path.join(__dirname, "../public")));
+app.set("views", path.join(__dirname, "../views"));
+
+app.set("view engine", "hbs");
+
+app.get("/", (req, res) => {
+  res.render("index", {
+    title: "Weather App",
+    name: "Andrew Mead",
+  });
+});
 
 app.get("/weather", (req, res) => {
   res.send({
@@ -15,6 +22,8 @@ app.get("/weather", (req, res) => {
     forecast: "raining",
   });
 });
+
+const PORT = 3000;
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);

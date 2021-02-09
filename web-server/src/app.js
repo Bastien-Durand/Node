0781cadc8eb8dade8ba930a032b461e0dc3c8f92
Bastien Durand <basilexpeditions@gmail.com>
2021-02-09 11:28:31 +1100

    Added partials for learning

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -1,14 +1,20 @@
 const express = require("express");
 const path = require("path");
+const hbs = require("hbs");
 
 const app = express();
 
-//This is the root being served as a static directory
+// Define paths for Express config
 const publicDirectoryPath = path.join(__dirname, "../public");
-const viewsPath = path.join(__dirname, "../templates");
+const viewsPath = path.join(__dirname, "../templates/views");
+const partialsPath = path.join(__dirname, "../templates/partials");
 
+// Setup handlebars engine and views location
 app.set("view engine", "hbs");
 app.set("views", viewsPath);
+hbs.registerPartials(partialsPath);
+
+// Setup static directory to serve
 app.use(express.static(publicDirectoryPath));
 
 app.get("/", (req, res) => {

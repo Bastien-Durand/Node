541fa1a312e9d2ba7bab3b58352a7cae6d41c761
Bastien Durand <basilexpeditions@gmail.com>
2021-02-09 11:04:41 +1100

    modified static hbs files to become dynamic

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -4,10 +4,12 @@ const path = require("path");
 const app = express();
 
 //This is the root being served as a static directory
-app.use(express.static(path.join(__dirname, "../public")));
-app.set("views", path.join(__dirname, "../views"));
+const publicDirectoryPath = path.join(__dirname, "../public");
+const viewsPath = path.join(__dirname, "../templates");
 
 app.set("view engine", "hbs");
+app.set("views", viewsPath);
+app.use(express.static(publicDirectoryPath));
 
 app.get("/", (req, res) => {
   res.render("index", {

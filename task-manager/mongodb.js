a500f55a3c35c547bf38c4cc455ed39f0f668c3b
Bastien Durand <basilexpeditions@gmail.com>
2021-02-23 12:15:14 +1100

    Challenges completed for Creating and Reading into DB --  CR of CRUD

--- task-manager/mongodb.js
+++ task-manager/mongodb.js
@@ -1,10 +1,17 @@
 // CRUD operations
 
-const mongodb = require("mongodb");
-const MongoClient = mongodb.MongoClient;
+// const mongodb = require("mongodb");
+// const MongoClient = mongodb.MongoClient;
+// const ObjectID = mongodb.ObjectID;
+
+const { MongoClient, ObjectID } = require("mongodb");
 
 const connectionURL = "mongodb://127.0.01:27017";
 const databaseName = "task-manager";
+
+const id = new ObjectID();
+console.log(id.id.length);
+console.log(id.toHexString().length);
 
 MongoClient.connect(
   connectionURL,
@@ -15,12 +22,70 @@ MongoClient.connect(
     }
     const db = client.db(databaseName);
 
+    // FIND SINGLE TASK BY OBJECTID
+
+    // db.collection("tasks").findOne(
+    //   { _id: new ObjectID("602c5bd1e1c3e15e058a4dde") },
+    //   (error, task) => {
+    //     if (error) {
+    //       console.log(error);
+    //     }
+    //     console.log("Task found by ID");
+    //     console.log(task);
+    //   }
+    // );
+
+    // FIND COMPLETED TASKS IN COLLECTION RETURN AS ARRAY
+
+    // db.collection("tasks")
+    //   .find({ completed: false })
+    //   .toArray((error, tasks) => {
+    //     if (error) {
+    //       console.log(error);
+    //     }
+    //     console.log("Tasks not yet complete:");
+    //     console.log(tasks);
+    //   });
+
+    // .find users with age 26 in DB and return in an array the collections
+
+    // db.collection("users")
+    //   .find({ age: 26 })
+    //   .toArray((error, users) => {
+    //     if (error) {
+    //       return console.log("Unable to fetch collection users");
+    //     }
+    //     console.log(users);
+    //   });
+
+    // db.collection("users").findOne(
+    //   { _id: new ObjectID("602b47ab61bcb75b45ef22b2") },
+    //   (error, user) => {
+    //     if (error) {
+    //       return console.log("Unable to fetch");
+    //     }
+    //     console.log(user);
+    //   }
+    // );
+
+    // .count find the number of users without storing in memory the users within an array
+
+    // db.collection("users")
+    //   .find({ age: 26 })
+    //   .count((error, count) => {
+    //     if (error) {
+    //       return console.log("Unable to fetch collection users");
+    //     }
+    //     console.log(count);
+    //   });
+
     // INSERT ONE
 
     // db.collection("users").insertOne(
     //   {
-    //     name: "Bastien",
-    //     age: 26,
+    //     _id: id,
+    //     name: "Vikram",
+    //     age: 28,
     //   },
     //   (error, result) => {
     //     if (error) {
@@ -31,7 +96,7 @@ MongoClient.connect(
     //   }
     // );
 
-    // INSERT MANY
+    // INSERT MANY #1
 
     // db.collection("users").insertMany(
     //   [
@@ -53,29 +118,31 @@ MongoClient.connect(
     //   }
     // );
 
-    db.collection("tasks").insertMany(
-      [
-        {
-          description: "Study up on mongoDB",
-          completed: false,
-        },
-        {
-          description: "Create a slack bot",
-          completed: false,
-        },
-        {
-          description: "Meet Sam to discuss antler idea",
-          completed: true,
-        },
-      ],
-      (error, result) => {
-        if (error) {
-          return console.log("There was an error writing to database");
-        }
+    // INSERT MANY #2
 
-        console.log(result.ops);
-      }
-    );
+    // db.collection("tasks").insertMany(
+    //   [
+    //     {
+    //       description: "Study up on mongoDB",
+    //       completed: false,
+    //     },
+    //     {
+    //       description: "Create a slack bot",
+    //       completed: false,
+    //     },
+    //     {
+    //       description: "Meet Sam to discuss antler idea",
+    //       completed: true,
+    //     },
+    //   ],
+    //   (error, result) => {
+    //     if (error) {
+    //       return console.log("There was an error writing to database");
+    //     }
+
+    //     console.log(result.ops);
+    //   }
+    // );
   }
 );
 
